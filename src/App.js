e9371d47b04509ce9c7bd6bbdca0098a8d916ea6
RazvanBanateanu <banateanu.razvan1@gmail.com>
2023-06-12 10:20:13 +0300

    Portofolio v2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Nav/>}>
+          <Route path='/Portofolio/' element={<Nav/>}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="skills" element={<Skills />} />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,10 +10,10 @@ const Nav = () => {
       <nav className="nav">
         <div className="logo">B.R.</div>
         <ul className="navbar">
-            <li><Link to="/" className="link">Home</Link></li>
-            <li><Link to="/about" className="link">About</Link></li>
-            <li><Link to="/skills" className="link">Skills</Link></li>
-            <li><Link to="/contact" className="link">Contact</Link></li>
+            <li><Link to="/Portofolio/" className="link">Home</Link></li>
+            <li><Link to="about" className="link">About</Link></li>
+            <li><Link to="skills" className="link">Skills</Link></li>
+            <li><Link to="contact" className="link">Contact</Link></li>
         </ul>
       </nav>
 
